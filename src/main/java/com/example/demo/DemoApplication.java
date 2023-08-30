8ed92ba958c8517527c615a6a6d69fd18afbe7c5
peterkopis <peter.pis@centrum.sk>
2023-08-30 13:29:51 +0200

    add the first controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -10,10 +11,15 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		System.out.printf("hola");
+		System.out.println("hola");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/greet")
+	public GreetResponse greet2(){
+		return new GreetResponse("hello Everybody",22);
+	}
 
+	record GreetResponse(String greet,int i){}
 
 }
